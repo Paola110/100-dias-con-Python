3b8f40d4690f6973bb05560f9db7ce7ccdd7ab20
Paola <montserrat_osorio@yahoo.com>
2024-07-07 16:31:27 -0600

    Dia 6 terminado, proyecto dia 6 terminado

---
+++ Dia6_Funciones_BloquesDeCodigo_While.py
@@ -0,0 +1,20 @@
+# Uso de funciones, bloques de codigo y while loops
+
+# Definir funciones
+
+def nombre_funcion():
+    print("1er paso.")
+    print("2do paso.")
+    print("3er paso.")
+
+nombre_funcion() # Llamar una funcion 
+
+# While loop
+
+something_is = 5 < 6
+
+while something_is == True:
+    print("Do this")
+    something_is = 6 < 5
+
+ 
