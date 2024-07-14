c603ec38843f24ae437258e7cddd97e19ae02b94
Paola <montserrat_osorio@yahoo.com>
2024-07-14 17:47:28 -0600

    Dia 9 a la mitad

--- Dia9_Diccionario_Anidacion.py
+++ Dia9_Diccionario_Anidacion.py
@@ -71,7 +71,52 @@ print ("\n -----------------------\n")
 # Anidaciones
 
 Mexico = {
-  "Ags": "municipios",
-  "Gto": "municipios",
-  "SLP": "municipios",
+  "Ags":
+  {
+    "municipios": ["JM", "RR", "C"],
+    "cantidad_mun": 17
+  },
+
+  "Gto": [ ["M1","M2", "M3"],21 ],
+
+  "SLP": [
+    { "municipios":["M1","M2", "M3"],"cantidad_mun":23} ,
+    2.3,
+    "Capital"
+    ],
 }
+
+# Ejercicio
+
+country = input("Ingresa el nombre del pais: ") # Add country name
+visits = int(input("Ingresa el numero de visitas: ")) # Number of visits
+list_of_cities = eval(input("Ingresa la lista de ciudades: ")) # create list from formatted string
+
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log.
+
+def add_new_country(country, visits, list_of_cities):
+  append(travel_log)
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+
+#Brazil
+#2
+#["Sao Paulo", "Rio de Janeiro"]
