27746694ff25024eb9b426e93c9e4e4c196f07fe
Paola <montserrat_osorio@yahoo.com>
2024-08-16 22:15:37 -0600

    Comienzo día 10, funciones con outputs

---
+++ Dia10_Function_Output.py
@@ -0,0 +1,11 @@
+# Funciones y multiples valores de return
+
+def my_function():
+    result = 3*2
+    return result
+
+output = my_function()
+
+print(output)
+
+print ("\n -----------------------\n")
